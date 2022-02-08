ea71d3171487bb339a91f40d99e79acfde18a9cf
gitbit789 <28954292+gitbit789@users.noreply.github.com>
2022-02-08 09:45:42 -0600

    dev4_views_config

--- onlinecourse/urls.py
+++ onlinecourse/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
 
     #path(route='course/<int:pk>/enroll/', view=views.EnrollView.as_view(), name='enroll'),
     path(route='', view=views.CourseListView.as_view(), name='popular_course_list'),
-    #path(route='course/<int:pk>/', view=views.CourseDetailsView.as_view(), name='course_details'),
+    path(route='course/<int:pk>/', view=views.CourseDetailsView.as_view(), name='course_details'),
 
     # Authentication related urls
     path('registration/', views.registration_request, name='registration'),
