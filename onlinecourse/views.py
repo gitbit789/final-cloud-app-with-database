26723c39aadc997a18b02c868709c6a616514a83
gitbit789 <28954292+gitbit789@users.noreply.github.com>
2022-02-19 18:41:35 -0600

    Update views.py

--- onlinecourse/views.py
+++ onlinecourse/views.py
@@ -91,7 +91,6 @@ class CourseDetailsView(generic.DetailView):
     context_object_name = "course_details"
 
 
-path('<int:course_id>/enroll/', views.enroll, name='enroll'),
 
 def enroll(request, course_id):
     course = get_object_or_404(Course, pk=course_id)
