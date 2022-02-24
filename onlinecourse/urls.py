27a5a58220ecfcb56ed7494ffdc0c8303fdee07b
gitbit789 <28954292+gitbit789@users.noreply.github.com>
2022-02-24 09:28:08 -0600

    Update urls.py

--- onlinecourse/urls.py
+++ onlinecourse/urls.py
@@ -6,7 +6,7 @@ from . import views
 app_name = 'onlinecourse'
 urlpatterns = [
 
-    path(route='course/<int:pk>/enroll/', view=views.EnrollView.as_view(), name='enroll'),
+    
     path(route='', view=views.CourseListView.as_view(), name='popular_course_list'),
     path(route='course/<int:pk>/', view=views.CourseDetailsView.as_view(), name='course_details'),
 
